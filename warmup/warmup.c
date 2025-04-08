ec71c512961005901c96bda2861d958c6f43718b
Brayan Pereira da Silva <108839358+Brayan-Pereira@users.noreply.github.com>
2025-04-07 23:34:36 -0300

    Update warmup.c

--- warmup/warmup.c
+++ warmup/warmup.c
@@ -4,17 +4,16 @@
 
 #include "include/warmup_solver.h"
 
-const char WARMUP_INSTANCE[] = "01-tecla-king/";
+const char WARMUP_INSTANCE[] = "02-aritmetica-morse/";
 const char INPUT_DIR[] = "input/";
 const char FILE_PREFIX[] = "instance_";
 
 int main() {
-
     DIR *dir;
     FILE *fptr;
-    int success, total_success, total_failures;
+    int total_success = 0, total_failures = 0;
     struct dirent *str_dir;
-    char input_dir[100], file_path[100];
+    char input_dir[200], file_path[200];
 
     input_dir[0] = '\0';
     strcat(input_dir, WARMUP_INSTANCE);
@@ -23,33 +22,27 @@ int main() {
     dir = opendir(input_dir);
 
     if (dir) {
-
         int prefix_length = strlen(FILE_PREFIX);
-        total_success = total_failures = 0;
 
-        while ((str_dir  = readdir(dir)) != NULL) {
-
-            if (strncmp(str_dir->d_name, FILE_PREFIX, prefix_length)) {
+        while ((str_dir = readdir(dir)) != NULL) {
+            if (strncmp(str_dir->d_name, FILE_PREFIX, prefix_length) != 0) {
                 continue;
             }
 
             file_path[0] = '\0';
-            strcat(file_path, WARMUP_INSTANCE);
-            strcat(file_path, INPUT_DIR);
+            strcat(file_path, input_dir);
             strcat(file_path, str_dir->d_name);
 
             fptr = fopen(file_path, "r");
             if (fptr == NULL) {
-                printf("File '%s' can't be opened\n", file_path);
-                return 1;
+                printf("Erro ao abrir o arquivo '%s'\n", file_path);
+                total_failures++;
+                continue;
             }
 
-            solve_warmup(fptr, str_dir->d_name, WARMUP_INSTANCE);
-            success = check_warmup_solution(str_dir->d_name, WARMUP_INSTANCE);
+            printf("Resolvendo: %s\n", str_dir->d_name);
 
-            printf("Test %s %s.\n", str_dir->d_name, success ? "SUCCESS" : "FAILURE");
-
-            if (success) {
+            if (solve_warmup(fptr, str_dir->d_name, WARMUP_INSTANCE)) {
                 total_success++;
             } else {
                 total_failures++;
@@ -58,9 +51,13 @@ int main() {
             fclose(fptr);
         }
 
-        printf("Total Success: %d\nTotal Failures: %d\n", total_success, total_failures);
+
+        printf("Total de sucessos: %d\n", total_success);
+        printf("Total de falhas: %d\n", total_failures);
 
         closedir(dir);
+    } else {
+        printf("Erro ao abrir o diretório: %s\n", input_dir);
     }
 
     return 0;
